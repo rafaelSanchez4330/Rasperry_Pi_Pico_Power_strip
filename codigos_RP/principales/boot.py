10cc57722b6c593654892dfecadabcdcc91bc985
Rafael Sánchez Saucedo <rafaelsanchez9924@gmail.com>
2025-11-10 13:54:48 -0600

    Implement WiFi connection in boot.py
    Add WiFi connection logic with LED indication

--- codigos_RP/principales/boot.py
+++ codigos_RP/principales/boot.py
@@ -1,1 +1,40 @@
+import utime
+from machine import Pin
+import network
+import gc
+
+led = Pin('LED', Pin.OUT)
+led.on()
+utime.sleep(1)
+# Define cuántos segundos esperar
+max_wait = 10 
+
+ssid = 'RedPrueba'
+password = '12345678'
+
+station = network.WLAN(network.STA_IF)
+station.active(True)
+station.connect(ssid, password)
+
+print("Conectando a WiFi...")
+while max_wait > 0:
+    if station.isconnected():
+        break # ¡Éxito! Salir del bucle
+        
+    max_wait -= 1
+    print('.')
+    utime.sleep(1) # Esperar 1 segundo
+
+# Comprobar si salimos por éxito o por timeout
+if station.isconnected():
+    print('\n¡Conexión exitosa!')
+    print(station.ifconfig())
+    led = Pin('LED', Pin.OUT)
+    led.on()
+else:
+    print('\nFallo al conectar.')
+
+gc.collect()
+
+
 
